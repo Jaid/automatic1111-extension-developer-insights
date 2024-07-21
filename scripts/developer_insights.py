d2f4a90a5a57ea7f42c04cba960c983e6801ac50
Jaid <6216144+Jaid@users.noreply.github.com>
2024-07-21 20:41:10 +0200

    Removed unneeded code copied over from the original script
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- scripts/developer_insights.py
+++ scripts/developer_insights.py
@@ -4,29 +4,6 @@ from modules.processing import StableDiffusionProcessing
 from lib.developer_insights.logger import logger
 from src.developer_insights.extension import extensionTitle
 
-templateBracketLeft = '{{'
-templateBracketRight = '}}'
-
-normalFontSize = 32 # for 1024x1024 pixels or anything with the same megapixel value
-hookType = 'postprocess_image'
-
-keysFromImg = [
-  'cfg_scale',
-  'width',
-  'height',
-  'seed',
-  'subseed',
-  'prompt',
-  'negative_prompt',
-]
-
-specialReplacements = {
-  'all_seeds': 'seed',
-  'all_subseeds': 'subseed',
-  'all_prompts': 'prompt',
-  'all_negative_prompts': 'negative_prompt',
-}
-
 class CustomTextOverlay(scripts.Script):
   def title(self):
     return extensionTitle
@@ -34,11 +11,7 @@ class CustomTextOverlay(scripts.Script):
   def show(self, is_img2img):
     return scripts.AlwaysVisible
 
-  def process(
-    self, processing: StableDiffusionProcessing, enabled: bool, textScale: int, textColor: str, backgroundColor: str, backgroundOpacity: int, paddingScale: int, marginScale: int, outlineScale: int, outlineColor: str, outlineOpacity: int, textEnabled1: bool, textEnabled2: bool, textEnabled3: bool,
-    textEnabled4: bool, textEnabled5: bool, textEnabled6: bool, textEnabled7: bool, textEnabled8: bool, textEnabled9: bool, textTemplate1: str, textTemplate2: str, textTemplate3: str, textTemplate4: str, textTemplate5: str, textTemplate6: str, textTemplate7: str, textTemplate8: str,
-    textTemplate9: str
-  ):
+  def process(self, processing: StableDiffusionProcessing):
     logger.info('process')
 
   def postprocess_image(self, processing: StableDiffusionProcessing, processed):
